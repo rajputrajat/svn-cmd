29ae5c4a1cac8ef3749b42638043a7cdf91c2900
Rajat Rajput <rajputrajat@gmail.com>
2021-06-29 22:51:05 +0530

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,65 @@
-use thiserror;
+#![deny(missing_docs)]
+#![deny(unsafe_code)]
+use std::result::Result;
+use thiserror::Error;
 
+/// Accessor to svn command functionality
 pub struct SvnCmd {}
 
-impl SvnCmd {
-    pub fn commit(&self) {}
-    pub fn checkout(&self) {}
-    pub fn update(&self) {}
-    pub fn log(&self) {}
-    // pub fn status(&self) -> SvnStatus {}
-    // pub fn info(&self) -> SvnInfo {}
+/// Builder to create SvnCmd
+pub struct SvnCmdBuilder {}
+
+impl SvnCmdBuilder {
+    pub fn commit_local_changes(&self) -> Result<(), SvnError> {
+        Ok(())
+    }
+
+    pub fn checkout(&self) -> Result<(), SvnError> {
+        Ok(())
+    }
+
+    pub fn update(&self) -> Result<(), SvnError> {
+        Ok(())
+    }
+
+    pub fn log(&self) -> Result<(), SvnError> {
+        Ok(())
+    }
+
+    pub fn status(&self) -> Result<SvnStatus, SvnError> {
+        Ok(SvnStatus {})
+    }
+
+    pub fn info(&self) -> Result<SvnInfo, SvnError> {
+        Ok(SvnInfo {})
+    }
+
+    pub fn delete(&self) -> Result<(), SvnError> {
+        Ok(())
+    }
+
+    pub fn revert(&self) -> Result<(), SvnError> {
+        Ok(())
+    }
+
+    pub fn copy_to(&self) -> Result<(), SvnError> {
+        Ok(())
+    }
+
+    pub fn switch_repo_locally(&self) -> Result<(), SvnError> {
+        Ok(())
+    }
+
+    pub fn merge() -> Result<(), SvnError> {
+        Ok(())
+    }
 }
 
 pub struct SvnInfo {}
 pub struct SvnStatus {}
+
+#[derive(Error, Debug)]
+pub enum SvnError {
+    #[error("no connectivity")]
+    Disconnection,
+}
