5cd4612607d4da5fb04660fe6816ae24b3466d9f
Rajat Rajput <rajputrajat@gmail.com>
2021-06-29 22:18:34 +0530

    will reserve this name of crates.io first

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use thiserror;
+
+pub struct SvnCmd {}
+
+impl SvnCmd {
+    pub fn commit(&self) {}
+    pub fn checkout(&self) {}
+    pub fn update(&self) {}
+    pub fn log(&self) {}
+    // pub fn status(&self) -> SvnStatus {}
+    // pub fn info(&self) -> SvnInfo {}
 }
+
+pub struct SvnInfo {}
+pub struct SvnStatus {}
