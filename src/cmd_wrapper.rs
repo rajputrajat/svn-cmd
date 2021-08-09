a7b43d65c9e49b8c04b04b1e819d6b275f296a1f
Rajat Rajput <rajputrajat@gmail.com>
2021-08-09 20:45:01 +0530

    make it testable

--- src/cmd_wrapper.rs
+++ src/cmd_wrapper.rs
@@ -4,13 +4,32 @@ use crate::errors::SvnError;
 use std::process::Command;
 
 /// cmd wrapper struct
-pub(crate) struct SvnWrapper;
+pub(crate) struct SvnWrapper {
+    cmd: String,
+}
 
 impl SvnWrapper {
-    pub(crate) fn svn_wrapper(args: &[&str]) -> Result<String, SvnError> {
-        match Command::new("svn").args(args).output() {
+    fn common_cmd_runner(&self, args: &[&str]) -> Result<String, SvnError> {
+        match Command::new(&self.cmd).args(args).output() {
             Ok(o) => String::from_utf8(o.stdout).map_err(|e| SvnError::FromUtf8Error { src: e }),
             Err(e) => Err(SvnError::MissingSvnCli { src: e }),
         }
     }
+}
+
+impl SvnWrapper {
+    pub(crate) fn new() -> Self {
+        Self {
+            cmd: "svn".to_owned(),
+        }
+    }
+
+    pub(crate) fn from_cmd<T: Into<String>>(cmd: T) -> Self {
+        Self { cmd: cmd.into() }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
 }
