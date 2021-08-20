7664bc500de2310e1764bda11dcb87ec4c3291f5
Rajat Rajput <rajputrajat@gmail.com>
2021-08-20 23:04:12 +0530

    keep reference

--- src/sub_commands/info.rs
+++ src/sub_commands/info.rs
@@ -78,8 +78,8 @@ pub(crate) struct SvnInfo {
 }
 
 impl SvnInfo {
-    pub(crate) fn parse<T: Into<String>>(xml: T) -> Self {
-        serde_xml_rs::from_str(&xml.into()).unwrap()
+    pub(crate) fn parse(xml: &str) -> Self {
+        serde_xml_rs::from_str(xml).unwrap()
     }
 }
 
