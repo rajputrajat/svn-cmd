fa1a8e3dc63454e69ba7fa229ae75a5fa1df4306
Rajat Rajput <rajputrajat@gmail.com>
2021-08-21 16:17:17 +0530

    add a test. it's working.

--- src/sub_commands/status.rs
+++ src/sub_commands/status.rs
@@ -80,3 +80,126 @@ where
     let s = String::deserialize(deserializer)?;
     StatusItemType::parse(s).map_err(de::Error::custom)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn status() {
+        let de = SvnStatus::parse(SVN_STATUS).unwrap();
+        println!("{:#?}", de);
+        assert!(false);
+    }
+
+    const SVN_STATUS: &str = r##" <?xml version="1.0" encoding="UTF-8"?>
+<status>
+    <target path=".">
+        <entry path="GDK\GDKBase">
+            <wc-status item="external" props="none"></wc-status>
+        </entry>
+        <entry path="GDK\Libraries">
+            <wc-status props="none" item="external"></wc-status>
+        </entry>
+        <entry path="GDK\Mercury\src\OpenGL.cpp">
+            <wc-status item="modified" revision="156377" props="none">
+                <commit revision="142706">
+                    <author>ddunford</author>
+                    <date>2020-03-02T19:40:25.301780Z</date>
+                </commit>
+            </wc-status>
+        </entry>
+        <entry path="GDK\cmake">
+            <wc-status props="none" item="external"></wc-status>
+        </entry>
+        <entry path="GDK\interface">
+            <wc-status props="none" item="external"></wc-status>
+        </entry>
+        <entry path="Games\.vs">
+            <wc-status item="unversioned" props="none"></wc-status>
+        </entry>
+        <entry path="Games\Binaries - Shortcut.lnk">
+            <wc-status item="unversioned" props="none"></wc-status>
+        </entry>
+        <entry path="Games\Bootstrap.exe - Debug.lnk">
+            <wc-status item="unversioned" props="none"></wc-status>
+        </entry>
+        <entry path="Games\Bootstrap.exe - Release.lnk">
+            <wc-status item="unversioned" props="none"></wc-status>
+        </entry>
+        <entry path="Games\BuffaloChief">
+            <wc-status item="unversioned" props="none"></wc-status>
+        </entry>
+        <entry path="Games\BuffaloChief.sln">
+            <wc-status item="unversioned" props="none"></wc-status>
+        </entry>
+        <entry path="Games\BuffaloChief.sln.lnk">
+            <wc-status item="unversioned" props="none"></wc-status>
+        </entry>
+        <entry path="Games\BuffaloGoldRevolution">
+            <wc-status item="unversioned" props="none"></wc-status>
+        </entry>
+        <entry path="Games\BuffaloGoldRevolution.sln">
+            <wc-status item="unversioned" props="none"></wc-status>
+        </entry>
+        <entry path="Games\ChoysKingdomDancingFoo">
+            <wc-status item="unversioned" props="none"></wc-status>
+        </entry>
+        <entry path="Games\ChoysKingdomDancingFoo.sln">
+            <wc-status item="unversioned" props="none"></wc-status>
+        </entry>
+        <entry path="Games\ChoysKingdomLunarFestival">
+            <wc-status item="unversioned" props="none"></wc-status>
+        </entry>
+        <entry path="Games\ChoysKingdomLunarFestival.sln">
+            <wc-status item="unversioned" props="none"></wc-status>
+        </entry>
+        <entry path="Games\Debug - Monaco Logs.lnk">
+            <wc-status props="none" item="unversioned"></wc-status>
+        </entry>
+        <entry path="Games\FacadeIntegritySuite.sln - Shortcut.lnk">
+            <wc-status props="none" item="unversioned"></wc-status>
+        </entry>
+        <entry path="Games\GDKRuntime_2015.sln - Shortcut.lnk">
+            <wc-status props="none" item="unversioned"></wc-status>
+        </entry>
+        <entry path="Games\GDK_2015.sln - Shortcut.lnk">
+            <wc-status item="unversioned" props="none"></wc-status>
+        </entry>
+        <entry path="Games\HHR_BuffaloGoldRevolution.sln - Shortcut.lnk">
+            <wc-status props="none" item="unversioned"></wc-status>
+        </entry>
+        <entry path="Games\Monaco bin - Debug.lnk">
+            <wc-status props="none" item="unversioned"></wc-status>
+        </entry>
+        <entry path="Games\Monaco bin - Release.lnk">
+            <wc-status item="unversioned" props="none"></wc-status>
+        </entry>
+        <entry path="Games\Monaco.sln - Shortcut.lnk">
+            <wc-status props="none" item="unversioned"></wc-status>
+        </entry>
+        <entry path="Games\fit">
+            <wc-status props="none" item="unversioned"></wc-status>
+        </entry>
+        <entry path="Games\reel_strip.patch">
+            <wc-status item="unversioned" props="none"></wc-status>
+        </entry>
+        <entry path="Games\runtime_before_drop_3.patch">
+            <wc-status props="none" item="unversioned"></wc-status>
+        </entry>
+        <entry path="Games\src.patch">
+            <wc-status item="unversioned" props="none"></wc-status>
+        </entry>
+        <entry path="GDK\GDKBase\GDKBase.vcxproj.user">
+            <wc-status item="unversioned" props="none"></wc-status>
+        </entry>
+        <entry path="GDK\Libraries\Common.vcxproj.user">
+            <wc-status props="none" item="unversioned"></wc-status>
+        </entry>
+        <entry path="GDK\interface\platform_interface">
+            <wc-status props="none" item="external"></wc-status>
+        </entry>
+    </target>
+</status>
+    "##;
+}
