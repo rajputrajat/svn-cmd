8c2197ca9cda7ab4a2bfd79c43bbf450463f671b
Rajat Rajput <rajputrajat@gmail.com>
2021-08-21 21:13:09 +0530

    svn log fetch values

--- src/sub_commands/log.rs
+++ src/sub_commands/log.rs
@@ -9,8 +9,33 @@ use serde::{
 use std::collections::VecDeque;
 
 #[derive(Debug)]
-pub struct SvnLog {
+pub struct SvnLog<F>
+where
+    F: Fn(u32) -> String,
+{
     queue: VecDeque<LogEntry>,
+    log_fetcher: F,
+}
+
+impl<F> SvnLog<F>
+where
+    F: Fn(u32) -> String,
+{
+    fn new(log_fetcher: F) -> Result<Self, SvnError> {
+        let logger = Self {
+            queue: VecDeque::new(),
+            log_fetcher,
+        };
+        logger.fetch(10)?;
+        Ok(logger)
+    }
+
+    fn fetch(&mut self, count: u32) -> Result<(), SvnError> {
+        let text = (self.log_fetcher)(count);
+        LogParser::parse(&text).map(|vl| {
+            self.queue.extend(vl.entry);
+        })
+    }
 }
 
 #[derive(Debug)]
@@ -30,15 +55,18 @@ pub struct LogEntry {
     msg: String,
 }
 
-impl Iterator for SvnLog {
+impl<F> Iterator for SvnLog<F>
+where
+    F: Fn(u32) -> String,
+{
     type Item = LogIter;
 
     fn next(&mut self) -> Option<Self::Item> {}
 }
 
-impl SvnLog {
-    fn new_count(count: u32) -> Self {
-        Self {}
+impl LogParser {
+    fn parse(text: &str) -> Result<Self, SvnError> {
+        serde_xml_rs::from_str::<Self>(text).map_err(|e| SvnError::Deserializer { src: e })
     }
 }
 
