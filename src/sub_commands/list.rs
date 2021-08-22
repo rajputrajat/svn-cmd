e0af27f172bd2c3b2caef5dc6d6a98a4be8ff6f4
Rajat Rajput <rajputrajat@gmail.com>
2021-08-22 20:57:28 +0530

    svn list is implemented. testing needs to be done

--- src/sub_commands/list.rs
+++ src/sub_commands/list.rs
@@ -1,18 +1,35 @@
-use crate::cmd_wrapper::SvnWrapper;
 use crate::errors::SvnError;
-use chrono::prelude::*;
-use serde::{
-    de::{self, Deserializer},
-    Deserialize,
-};
+use crate::sub_commands::info::EntryCommit;
+use serde::Deserialize;
+use std::collections::VecDeque;
 
-pub struct SvnList {}
+#[derive(Deserialize, Debug)]
+pub struct SvnList {
+    list: ListsList,
+}
 
-impl SvnList {}
+#[derive(Deserialize, Debug)]
+struct ListsList {
+    entries: VecDeque<ListEntry>,
+}
 
-pub struct ListEntry {}
+#[derive(Deserialize, Debug)]
+pub struct ListEntry {
+    name: String,
+    size: u32,
+    commit: EntryCommit,
+}
+
+impl SvnList {
+    pub(crate) fn parse(xml_text: &str) -> Result<Self, SvnError> {
+        serde_xml_rs::from_str::<Self>(xml_text).map_err(|e| SvnError::Deserializer { src: e })
+    }
+}
 
 impl Iterator for SvnList {
     type Item = ListEntry;
-    fn next(&mut self) -> Option<Self::Item> {}
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.list.entries.pop_front()
+    }
 }
